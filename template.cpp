328c7c896632fef03a2bb45ea14f137c40226821
SeChan Park <hasmi5452@gmail.com>
2023-04-25 12:25:37 +0900

    Update template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file template.cpp
+ * @author amkorousagi (hasmi5452@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-11-18 16:29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);
+#define endl '\n'
+
+int main(void) {
+	fastio;
+
+	return 0;
+}
